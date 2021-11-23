54fe8d91313eeb54003c48a54205cc8eb219cf3f
pgmagno <pgs.magno@gmail.com>
2021-11-23 18:56:28 -0300

    finaliza o jogo, cria novas classes para controle de turnos, o modo 2 jogadores é concluído

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Board {
 
     private String[][] board = new String[3][3];
     private int numberOfTurns = 0;
     private String[] playerSymbol = new String[2];
+    private String playerTurn;
 
 
     public static final String ANSI_RESET = "\u001B[0m";
@@ -37,40 +40,45 @@ public class Board {
         System.out.println();
     }
 
-    public void makeMove(String[] move, String playerSymbol) {
-        if (board[Integer.parseInt(move[0]) - 1][Integer.parseInt(move[1]) - 1].equals("   ")) {
-            board[Integer.parseInt(move[0]) - 1][Integer.parseInt(move[1]) - 1] = playerSymbol;
-        } else {
-            System.out.println(ANSI_RED + "Movimento não permitido" + ANSI_RESET);
+    public void drawFinalBoard(int num0, int num1, int num2, int num3, int num4, int num5) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (j == 2 && (i == 0 || i == 1 || i == 2)) {
+                    if((i == num0 && j == num1) || (i == num2 && j == num3) || (i == num4 && j == num5)) {
+                        System.out.print(ANSI_RED + board[i][j] + ANSI_RESET);
+                    } else {
+                        System.out.print(board[i][j]);
+                    }
+                } else {
+                    if ((i == num0 && j == num1) || (i == num2 && j == num3) || (i == num4 && j == num5)) {
+                        System.out.print(ANSI_RED + board[i][j] + ANSI_RESET + " | ");
+                    } else {
+                        System.out.print(board[i][j] + " | ");
+                    }
+                }
+            }
+            if (i != 2) {
+                System.out.println();
+                System.out.println("----+-----+----");
+            }
         }
+        System.out.println();
     }
 
-    public String moveSelector() {
 
-        String playerTurn;
-
-        if (numberOfTurns % 2 == 0) {
-            playerTurn = playerSymbol[0];
-        } else {
-            playerTurn = playerSymbol[1];
-        }
-        numberOfTurns++;
-
-        return playerTurn;
-    }
 
     public boolean gameCheck() {
 
         boolean gameEnded = false;
 
         int[][] combMatrix = {
-                {0, 0, 0, 1, 0, 2}, // linha superior
-                {1, 0, 1, 1, 1, 2}, // linha central
-                {2, 0, 2, 1, 2, 2}, // linha inferior
-                {0, 0, 1, 0, 2, 0}, // coluna esquerda
-                {0, 1, 1, 1, 2, 1}, // coluna central
-                {0, 2, 1, 2, 2, 2}, // coluna direita
-                {0, 0, 1, 1, 2, 2}, //diagonal decrescente
+                {0, 0, 0, 1, 0, 2}, //  linha superior
+                {1, 0, 1, 1, 1, 2}, //  linha central
+                {2, 0, 2, 1, 2, 2}, //  linha inferior
+                {0, 0, 1, 0, 2, 0}, //  coluna esquerda
+                {0, 1, 1, 1, 2, 1}, //  coluna central
+                {0, 2, 1, 2, 2, 2}, //  coluna direita
+                {0, 0, 1, 1, 2, 2}, //  diagonal decrescente
                 {0, 2, 1, 1, 2, 0}}; // diagonal crescente
 
         gameCheckLoop:
@@ -82,10 +90,47 @@ public class Board {
                         board[num[4]][num[5]].equals(symbol)) {
                     System.out.println(ANSI_ITABlue + "Winner: Player" + symbol + ANSI_RESET);
                     gameEnded = true;
+                    drawFinalBoard(num[0], num[1], num[2], num[3], num[4], num[5]);
                     break gameCheckLoop;
                 }
             }
         }
-    return gameEnded;
+        return gameEnded;
+    }
+
+    public void makeMove(String[] move, String playerSymbol) {
+        if (board[Integer.parseInt(move[0]) - 1][Integer.parseInt(move[1]) - 1].equals("   ")) {
+            board[Integer.parseInt(move[0]) - 1][Integer.parseInt(move[1]) - 1] = playerSymbol;
+            switchPlayer();
+            numberOfTurns++;
+        } else {
+            System.out.println(ANSI_RED + "Movimento não permitido" + ANSI_RESET);
+        }
+    }
+
+    public void randomChooser () {
+
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 3);
+        if (randomNum == 1) {
+            playerTurn = playerSymbol[0];
+        } else {
+            playerTurn = playerSymbol[1];
+        }
+    }
+
+    public void switchPlayer () {
+        if (playerTurn.equals(playerSymbol[0])) {
+            playerTurn = playerSymbol[1];
+        } else {
+            playerTurn = playerSymbol[0];
+        }
+    }
+
+    public int getNumberOfTurns () {
+        return numberOfTurns;
+    }
+
+    public String getPlayerTurn() {
+        return playerTurn;
     }
 }
--- src/GameController.java
+++ src/GameController.java
@@ -2,38 +2,49 @@ import java.util.Scanner;
 
 public class GameController {
 
-
-
     public static void main (String[] args){
         System.out.println("Bem-vindo ao Jogo da Velha");
         System.out.println("Digite no terminal uma combinação de \"linha coluna\"");
         System.out.println("Por exemplo: 1 1");
         System.out.println("Esse comando irá colocar o símbolo célula no canto esquerdo superior");
         System.out.println("Linhas e colunas vão de 1 a 3");
+        System.out.println("Decidam entre si a quem pertencerá os símbolos \"X\" ou \"O\".");
+        System.out.println("O jogo sorteará um dos símbolos para ser o primeiro a jogar.");
+        System.out.println("Pressione \"Enter\" quando estiverem prontos!");
+        Scanner readySetGo = new Scanner(System.in);
+        readySetGo.nextLine();
+
+
+
+
 
         Board myBoard = new Board();
         myBoard.drawBoard();
         Scanner playerInput = new Scanner(System.in);
 
+        String symbol;
+        myBoard.randomChooser();
+        String[] playerCommand;
+
         while(true) {
-            String symbol = myBoard.moveSelector();
-            System.out.print("Digite o movimento Jogador (" + symbol + ") : ");
-            String[] playerCommand = new String[2];
+            symbol = myBoard.getPlayerTurn();
+            System.out.print("Digite o movimento, Jogador (" + symbol + ") : ");
+
             try {
                 playerCommand = playerInput.nextLine().split(" ");
                 myBoard.makeMove(playerCommand, symbol);
-            } catch (ArrayIndexOutOfBoundsException exception) {
-                System.out.println("Digite o número certo");
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException exception) {
+                System.out.println("Digite a combinação correta de linha e coluna. Ex: 1 1");
             }
 
             myBoard.drawBoard();
 
-            if (myBoard.gameCheck()) {
-                System.out.println("Bye bye");
-                break;
+            if (myBoard.getNumberOfTurns() >= 5) {
+                if (myBoard.gameCheck()) {
+                    System.out.println("Obrigado por jogar!");
+                    break;
+                }
             }
-
-
         }
 
     }
